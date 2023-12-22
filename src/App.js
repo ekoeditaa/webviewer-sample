e251310234d965ff66f8e01aa494f5cb0702abcd
Eko Edita <ekoeditaa@gmail.com>
2023-12-22 11:38:18 +0800

    add code to reproduce bug
    Signed-off-by: Eko Edita <ekoeditaa@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,36 +1,85 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import WebViewer from "@pdftron/webviewer";
+import "./App.css";
 
 const App = () => {
   const viewer = useRef(null);
+  const [file, setFile] = useState(null);
 
-  // if using a class, equivalent of componentDidMount 
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-        licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
+        path: "/webviewer/lib",
+        initialDoc: "/files/sample.png",
+        licenseKey:
+          "demo:1699603141853:7cc2b0780300000000239187b4df38ae6bd5fbb91c45b60f3516857af0",
+        fullAPI: true,
       },
-      viewer.current,
+      viewer.current
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+      const { documentViewer } = instance.Core;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
+      const ZOOM_LEVEL = 2;
+
+      documentViewer.addEventListener("documentLoaded", async () => {
+        const doc = documentViewer.getDocument();
+        const currentPage = documentViewer.getCurrentPage();
+        const pageInfo = doc.getPageInfo(currentPage);
+        console.log("width after document loaded: ", pageInfo.width);
+        console.log("height after document loaded: ", pageInfo.height);
+
+        const pageCount = documentViewer.getPageCount();
+        const pagesToBeRemoved = [];
+        for (let i = 2; i <= pageCount; i++) {
+          pagesToBeRemoved.push(i);
+        }
+        const pageNumber = 1;
+        const zoom = ZOOM_LEVEL; // render at twice the resolution
+        await doc.removePages(pagesToBeRemoved);
+        doc.loadCanvas({
+          pageNumber,
+          zoom,
+          drawComplete: async (canvas) => {
+            canvas.toBlob((blob) => {
+              if (!blob) {
+                return;
+              }
+
+              const fr = new FileReader();
+
+              fr.onload = function() {
+                // file is loaded
+                const img = new Image();
+
+                img.onload = function() {
+                  console.log("width after canvas drawComplete: ", img.width); // image is loaded; sizes are available
+                  console.log("height after canvas drawComplete: ", img.height); // image is loaded; sizes are available
+                };
+
+                img.src = fr.result; // is the data URL because called with readAsDataURL
+              };
+
+              fr.readAsDataURL(blob);
+
+              const reader = new FileReader();
+              reader.onloadend = () => {
+                const binaryData = reader.result;
+
+                const resizedFile = new File(
+                  [binaryData],
+                  "resized_image.png",
+                  {
+                    type: "image/png",
+                  }
+                );
+                setFile(resizedFile);
+              };
+
+              reader.readAsArrayBuffer(blob);
+            }, "image/png");
+          },
         });
-
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
       });
     });
   }, []);
